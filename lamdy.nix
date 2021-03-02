3daa03a45dc416c24474471882a8f27f441c9b7f
li <@li:maisiliym.uniks>
2021-03-02 04:07:49 +0000

    argz

--- lamdy.nix
+++ lamdy.nix
@@ -1,4 +1,28 @@
-{ stdenv }:
+{ self
+, kor
+, krimyn
+, input
+, stdenv
+, writeText
+, vimPloginz
+, vimPlugins
+, neovim-remote
+, python3Packages
+, tree-sitter
+, tree-sitter-parsers
+, llvmPackages_latest
+, cmake-language-server
+, haskell-language-server
+, universal-ctags
+, rust-analyzer
+, rnix-lsp
+, nixpkgs-fmt
+, gopls
+, go
+, ghc
+, cabal-install
+, stack
+}:
 
 stdenv.mkDerivation {
   pname = "niovi";
